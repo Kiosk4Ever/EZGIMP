39012425121a1aceac913b731664901807055c80
Kiosk4Ever <132842292+Kiosk4Ever@users.noreply.github.com>
2025-06-10 20:25:52 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
 
